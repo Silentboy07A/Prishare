09ad0fd56bb819f5fb5e42a2019c58fc61e28086
Silentboy07A <sakthibalamurugan06@gmail.com>
2026-06-13 19:48:34 +0530

    implement user regrestration api

---
+++ src/main/java/com/prishare/backend/UserController.java
@@ -0,0 +1,16 @@
+package com.prishare.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
